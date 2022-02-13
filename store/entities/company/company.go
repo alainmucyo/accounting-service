97723305f2c3b45423c48aeebb080e8a7cf4b3a4
Alain MUCYO <alainmucyo3@gmail.com>
2022-02-13 11:27:39 +0200

    [Finishes #181193277] Add actual and available balance

--- store/entities/company/company.go
+++ store/entities/company/company.go
@@ -6,8 +6,9 @@ import (
 
 type Company struct {
 	entities.Base
-	CompanyID string `gorm:"type:varchar(50)" json:"company_id"`
-	Balance   int64  `gorm:"default:0" json:"balance"`
+	CompanyID        string `gorm:"type:varchar(50)" json:"company_id"`
+	ActualBalance    int64  `gorm:"default:0" json:"actual_balance"`
+	AvailableBalance int64  `gorm:"default:0" json:"available_balance"`
 }
 
 func (Company) TableName() string {
